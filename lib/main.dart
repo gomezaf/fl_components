459de344490283cc484e19f5416a860343a67628
AndresGomez <yosoyandresfelipe@gmail.com>
2023-09-22 17:45:13 -0300

    learn about App Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:fl_components/screens/screens.dart';
+import 'package:fl_components/router/app_routes.dart';
 
 void main() => runApp(const MyApp());
 
@@ -12,14 +12,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      initialRoute: 'home',
-      routes: {
-        'home': (BuildContext context) => const HomeScreen(),
-        'listview1': (BuildContext context) => const Listview1Screen(),
-        'listview2': (BuildContext context) => const Listview2Screen(),
-        'alert': (BuildContext context) => const AlertScreen(),
-        'card': (BuildContext context) => const CardScreen(),
-      },
+      initialRoute: AppRoutes.initialRoute,
+      routes: AppRoutes.routes,
     );
   }
 }
---
+++ lib/router/app_routes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import 'package:fl_components/screens/screens.dart';
+
+class AppRoutes {
+  static const initialRoute = 'home';
+  static Map<String, Widget Function(BuildContext)> routes = {
+    'home': (BuildContext context) => const HomeScreen(),
+    'listview1': (BuildContext context) => const Listview1Screen(),
+    'listview2': (BuildContext context) => const Listview2Screen(),
+    'alert': (BuildContext context) => const AlertScreen(),
+    'card': (BuildContext context) => const CardScreen(),
+  };
+}
