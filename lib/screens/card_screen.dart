fa9eeecfb42cdd4b1b8867c1011f77930d7af3b7
AndresGomez <yosoyandresfelipe@gmail.com>
2023-10-04 15:18:09 -0300

    add card screen

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
 
+import 'package:fl_components/theme/app_theme.dart';
+
 class CardScreen extends StatelessWidget {
   const CardScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('CardScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(child: Text('CardScreen')),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        children: const [
+          Card(
+            child: Column(
+              children: [
+                ListTile(
+                  leading: Icon(
+                    Icons.photo_library_outlined,
+                    color: AppTheme.primary,
+                  ),
+                  title: Text('Soy una foto'),
+                  subtitle: Text('Estoy describiendo la foto anterior'),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
